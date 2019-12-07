4a6355a8959fb06257d37f5dd8f0716c894b4a92
Prateek Barapatre <pbarapatre@vmware.com>
2019-12-07 19:54:56 +0530

    initial commit

---
+++ src/main/java/com/shack/app/error/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.shack.app.error;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(Long id) {
+        super("Book id not found : " + id);
+    }
+
+}
---
+++ src/main/java/com/shack/app/error/BookUnSupportedFieldPatchException.java
@@ -0,0 +1,11 @@
+package com.shack.app.error;
+
+import java.util.Set;
+
+public class BookUnSupportedFieldPatchException extends RuntimeException {
+
+    public BookUnSupportedFieldPatchException(Set<String> keys) {
+        super("Field " + keys.toString() + " update is not allow.");
+    }
+
+}
